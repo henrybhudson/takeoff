7e48951c2abcc654c16eaddf8ac2c313f36e948c
Henry Hudson <henry.b.hudson@durham.ac.uk>
2024-03-24 18:45:10 +0000

    Study page start + graphics

---
+++ client/src/components/Public.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+import React from 'react'
+
+const Public = () => {
+        const content = (<div></div>);
+        return content;
+};
+
+export default Public;
